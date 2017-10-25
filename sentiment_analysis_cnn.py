aa73aa3f0c2366ba6071bcaa6752e73edebc8667
Hamza EL ACHOURI <hamza.elachouri@gmail.com>
2017-10-25 23:57:36 +0100

    Add files via upload

---
+++ sentiment_analysis_cnn.py
@@ -0,0 +1,42 @@
+# CNN for sentiment analysis using the Keras IMDB reviews dataset
+
+import numpy as np
+from keras.datasets import imdb
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.layers.convolutional import Conv1D
+from keras.layers.convolutional import MaxPooling1D
+from keras.layers.embeddings import Embedding
+from keras.preprocessing import sequence
+
+# fix random seed for reproducibilty
+seed = 7
+np.random.seed(seed)
+
+# load the data set but only keep the top 5000 words, zero the rest
+top_words = 5000
+(X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=top_words)
+
+# pad dataset to a maximum review length in words
+max_words = 500
+X_train = sequence.pad_sequences(X_train, maxlen=max_words)
+X_test = sequence.pas_sequences(X_test, maxlen=max_words)
+
+# create the model
+model = Sequential()
+model.add(Embedding(tp_words, 32, input_length=max_words))
+model.add(Conv1D(filters=32, kernel_size=3, padding='same', activation='relu'))
+model.add(MaxPooling1D(pool_size=2))
+model.add(Flatten())
+model.add(Dense(250, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+print(model.summary())
+
+# fit the model
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=2, batch_size=128, verbose=2)
+
+#final evaluation of the model
+scores = model.evaluate(X_test, y_test, verbose=0)
+print('Accuracy: %.2f%%' % (scores[1]*100))
---
+++ sentiment_analysis_mlp.py
@@ -0,0 +1,36 @@
+# Multi-layer perceptron for sentiment analysis using the Keras IMDB reviews dataset
+
+import numpy as np
+from keras.dataset import imdb
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.layers.embeddings. import Embedding
+from keras.preprocessing import sequence
+
+# fix random seed for reproducibility
+seed = 7
+np.random.seed(seed)
+
+# load the dataset but only keep the top 5000 words, zero the rest
+top_words = 5000
+(X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=top_words)
+max_words = 500
+X_train = sequence.pad_sequences(X_train, max_len=max_words)
+X_test = sequence.pad_sequences(X_test, max_len=max_words)
+
+# create the model
+model = Sequential()
+model.add(Embedding(top_words, 32, input_length=max_words))
+model.add(Flatten())
+model.add(Dense(250, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+print(model.summary())
+
+# fit the model
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=2, batch_size=128, verbose=2)
+
+# final evaluation of the model
+scores = model.evaluate(X_test, y_test, verbose=0)
+print('Accuracy: %.f%%' % (scores[1]*100))
