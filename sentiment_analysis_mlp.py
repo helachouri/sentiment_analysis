f13c9b5d088d22f5722e4654f00f9017ff367703
Hamza EL ACHOURI <hamza.elachouri@gmail.com>
2017-10-26 22:23:14 +0100

    Update sentiment_analysis_mlp.py

--- sentiment_analysis_mlp.py
+++ sentiment_analysis_mlp.py
@@ -1,11 +1,11 @@
 # Multi-layer perceptron for sentiment analysis using the Keras IMDB reviews dataset
 
 import numpy as np
-from keras.dataset import imdb
+from keras.datasets import imdb
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Flatten
-from keras.layers.embeddings. import Embedding
+from keras.layers.embeddings import Embedding
 from keras.preprocessing import sequence
 
 # fix random seed for reproducibility
@@ -15,9 +15,11 @@ np.random.seed(seed)
 # load the dataset but only keep the top 5000 words, zero the rest
 top_words = 5000
 (X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=top_words)
+
+# pad dataset to a maximum review length in words
 max_words = 500
-X_train = sequence.pad_sequences(X_train, max_len=max_words)
-X_test = sequence.pad_sequences(X_test, max_len=max_words)
+X_train = sequence.pad_sequences(X_train, maxlen=max_words)
+X_test = sequence.pad_sequences(X_test, maxlen=max_words)
 
 # create the model
 model = Sequential()
@@ -33,4 +35,4 @@ model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=2, batch_si
 
 # final evaluation of the model
 scores = model.evaluate(X_test, y_test, verbose=0)
-print('Accuracy: %.f%%' % (scores[1]*100))
+print('Accuracy: %.2f%%' % (scores[1]*100))
